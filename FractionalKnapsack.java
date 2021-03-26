c0ef1324da9acec06e7fe658566bb85fadbe71ee
Rajat <59741387+evilninja6726@users.noreply.github.com>
2021-03-26 23:34:52 +0530

    Add files via upload
    Fractional Knapsack Problem where items are divisible and we have to make the value maximum in O(nlogn) Time and O(1) Space.

---
+++ FractionalKnapsack.java
@@ -0,0 +1,41 @@
+package greedyAlgorithms;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FractionalKnapsack {
+    public static void main(String[] args) {
+        double x = fractionalKnapsack(new WeightValue[]{new WeightValue(50, 600), new WeightValue(20, 500), new WeightValue(30, 400)}, 70);
+        System.out.println(x);
+    }
+
+    public static double fractionalKnapsack(WeightValue[] pair, int capacity) {
+        Arrays.sort(pair, new Comparator<WeightValue>() {
+            @Override
+            public int compare(WeightValue o1, WeightValue o2) {
+                return (o2.value * o1.weight) - (o1.value * o2.weight);
+            }
+        });
+        double res = 0;
+        for (int i = 0; i < pair.length; i++) {
+            if (capacity >= pair[i].weight) {
+                res += pair[i].value;
+                capacity -= pair[i].weight;
+            } else {
+                res += (pair[i].value * capacity) / pair[i].weight;
+                return res;
+            }
+        }
+        return res;
+    }
+}
+
+class WeightValue {
+    int weight;
+    int value;
+
+    WeightValue(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+}
